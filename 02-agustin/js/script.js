ee906332a1fb25720de99860c94aa2801177a031
George <lets.devel0pme@gmail.com>
2023-09-06 19:01:21 -0300

    Home updated: title and p's

---
+++ 02-agustin/js/script.js
@@ -0,0 +1,50 @@
+
+
+console.log('hi')
+
+
+/* let a = 4;
+let b = 2;
+let result = a + b;
+
+
+let sum = a + b;
+console.log(sum); */
+
+let dog = {
+    name: 'firulais',
+    age: 10 + ' years old',
+    gender: 'male',
+    location: 'santiago',
+}
+console.log(dog.age);
+
+
+function multiplication(num1, num2) {
+    let result = num1 * num2;
+
+        return `your multiplication is ${result}`;
+}
+
+console.log(multiplication(30, 10));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
