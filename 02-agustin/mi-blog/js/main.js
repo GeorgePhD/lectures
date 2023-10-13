e9e3529c2c240bda3bdbc3c51562edc3f7afb0d6
George <lets.devel0pme@gmail.com>
2023-10-13 15:03:22 -0300

    folders modified

---
+++ 02-agustin/mi-blog/js/main.js
@@ -0,0 +1,26 @@
+//this comments one line only
+/* this comments a piece of text */
+
+console.log('hello');
+//alert('hello');
+//confirm('are you sure you want to download the file?');
+
+/* let userAge = prompt('enter your age');
+
+if (userAge  >= 18) {
+
+    alert('you are an adult');
+
+} else if (userAge < 18) {
+    alert('you are underage');
+} */
+
+
+
+
+
+
+
+
+
+
