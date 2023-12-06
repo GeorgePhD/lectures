529b3c2b362855e0a156c75c60d6bdaedc8140aa
George <lets.devel0pme@gmail.com>
2023-12-06 18:00:46 -0300

    styles added

--- 02-agustin/mi-blog/js/main.js
+++ 02-agustin/mi-blog/js/main.js
@@ -4,12 +4,20 @@ console.log("Hello World!");
 const body = document.querySelector("body");
 const icon = document.querySelector("#icon");
 function changeMode() {
-    document.body.classList.toggle("dark-mode");
+    body.classList.toggle("dark-mode");
+
+    const isDarkMode = body.classList.contains("dark-mode");
+
+    localStorage.setItem("dark-mode", isDarkMode);
 }
 
 icon.addEventListener("click", changeMode);
 
+const saveMode = localStorage.getItem("dark-mode");
 
+if (saveMode !== null && saveMode === "true") {
+    body.classList.add("dark-mode");
+}
 
 
 
