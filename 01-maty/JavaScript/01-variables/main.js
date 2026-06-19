eb59394570d1b046da3ae3066e9c1b25de757345
Jorge Gamboa <j.gamboafernandez@gmail.com>
2026-06-19 17:55:03 -0400

    folders variables&operators added

---
+++ 01-maty/JavaScript/01-variables/main.js
@@ -0,0 +1,63 @@
+
+//data type 
+
+//1. string
+
+let name = "Jorge"; //let se puede cambiar
+const lastName = "Gamboa"; // const no te deja cambiar
+let lives = "Don Sebastián de Rauquén, Curicó";
+let email = "j.gamboafernandez@gmail.com"
+//2. boolean
+let isStudent = false;
+
+
+
+//3. Number
+let age = 42;
+let cellphoneNumber = 56946410992;
+
+//4. undefined
+let pcGamer; 
+
+//5. null
+let pcgamer2 = null;
+//6. abject
+
+
+//7, array
+const animals = ["dog", "cat", "elephant"];
+
+
+let text = `Mi nimbre es ${name} y mi apellido es ${lastName}. Tengo ${age} años de edad, mi número celular es ${cellphoneNumber}. Mi email es el siguiente: ${email} Vivo en ${lives}. Soy un estudiante? la respuesta es ${isStudent}. tengo pc game? = ${pcGamer} tengo un pcgamer 2: ${pcgamer2}`
+
+console.log(text);
+console.log(animals[0]);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
